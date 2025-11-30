6f565079e5fab9b07417050054feb7c270babb2b
Ana Luz <anacarlabatista13@gmail.com>
2025-11-29 21:02:11 -0300

    primeira atualização

---
+++ DesafioLogica1.js
@@ -0,0 +1,17 @@
+
+let nomeheroi = "superman"
+let XP = 500
+let nivel = ""
+
+
+if (XP <=1000) {
+    nivel = "Ferro"
+}
+
+if (XP >=1001 && XP <=2000 ) {
+    nivel = "Bronze"
+}
+
+
+//saída
+console.log("O Herói de nome" + nomeheroi + "está no nível de" + nivel + ".")
