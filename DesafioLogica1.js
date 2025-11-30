960b4c455a14a6d2ec6f7b4dbd02fe0338ad7aca
Ana Luz <anacarlabatista13@gmail.com>
2025-11-29 21:23:33 -0300

    atualização

--- DesafioLogica1.js
+++ DesafioLogica1.js
@@ -1,8 +1,8 @@
 
-let nomeheroi = "superman"
-let XP = 500
+//variaveis
+let nomeheroi = "Superman"
+let XP = 8000
 let nivel = ""
-
 
 if (XP <=1000) {
     nivel = "Ferro"
@@ -12,6 +12,31 @@ if (XP >=1001 && XP <=2000 ) {
     nivel = "Bronze"
 }
 
+if (XP >=2001 && XP <=5000 ) {
+    nivel = "Prata"
+}
+
+if (XP >=5001 && XP <=7000 ) {
+    nivel = "Ouro"
+}
+
+if (XP >=7001 && XP <=8000 ) {
+    nivel = "Platina"
+
+}
+
+if (XP >=8001 && XP <=9000 ) {
+    nivel = "Ascendente"
+}
+
+if (XP >=9001 && XP <=10000 ) {
+    nivel = "Imortal"
+}
+
+if (XP >=10001 ) {
+    nivel = "Radiante"
+}
+
 
 //saída
-console.log("O Herói de nome" + nomeheroi + "está no nível de" + nivel + ".")
+console.log(" O Herói de nome " + nomeheroi + " está no nível de  " + nivel + " . ")
